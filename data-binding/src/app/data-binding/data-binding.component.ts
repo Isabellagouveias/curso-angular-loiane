52579fdee88d660dd892b814ddf25118d2c76945
isabellagouveias <isabellagouveias@gmail.com>
2023-10-02 12:38:46 -0300

    feat: property binding e interpolação

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css'],
+})
+export class DataBindingComponent {
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem =
+    'https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png';
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+}
