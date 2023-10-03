bbe71a51a7072a7287e335e83066da78abff431b
isabellagouveias <isabellagouveias@gmail.com>
2023-10-02 22:51:22 -0300

    feat: event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,18 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css'],
+  //styleUrls: ['./data-binding.component.css'],
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    `,
+  ],
 })
 export class DataBindingComponent {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem =
     'https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png';
+  valorAtual: string = '';
+  valorSalvo = '';
+  isMouseOver: boolean = false;
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    const inputElement = evento.target as HTMLInputElement;
+    this.valorAtual = inputElement.value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 }
