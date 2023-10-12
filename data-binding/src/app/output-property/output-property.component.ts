c42959bfd28ddd6af209ff01b31cdc8b1d63aa27
isabellagouveias <isabellagouveias@gmail.com>
2023-10-12 20:29:37 -0300

    feat: acesso ao dom e ao template com viewchild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,15 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'contador',
   templateUrl: './output-property.component.html',
   styleUrls: ['./output-property.component.css'],
 })
-export class OutputPropertyComponent {
+export class OutputPropertyComponent implements AfterViewInit {
   @Input() valor: number = 0;
-
   @Output() mudouValor = new EventEmitter();
+
+  @ViewChild('campoInput') campoValorInput!: ElementRef;
+
+  ngAfterViewInit() {
+    console.log('this.campoValorInput', this.campoValorInput);
+  }
+
   incrementa() {
+    console.log(
+      'this.campoValorInput.nativeElement.value',
+      this.campoValorInput.nativeElement.value
+    );
+
     this.valor++;
     this.mudouValor.emit({ novoValor: this.valor });
   }
