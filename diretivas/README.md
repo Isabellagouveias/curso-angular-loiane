854b6a7f739a4ba42a097442c16de8484275026a
isabellagouveias <isabellagouveias@gmail.com>
2023-10-12 20:48:21 -0300

    feat: angular cli - criando componente e service

--- diretivas/README.md
+++ diretivas/README.md
@@ -1,4 +1,4 @@
-# Diretias
+# diretivas
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.2.
 
--- diretivas/src/app/app.component.spec.ts
+++ diretivas/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'diretias'`, () => {
+  it(`should have as title 'diretivas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('diretias');
+    expect(app.title).toEqual('diretivas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('diretias app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('diretivas app is running!');
   });
 });
